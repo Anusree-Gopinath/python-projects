e21b50342f36a1dc899824f8de9601d7daf4ab2c
Anusree-Gopinath <53139990+Anusree-Gopinath@users.noreply.github.com>
2026-01-05 22:30:50 +0000

    Fixed issue with csv

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # Step 1: Import pandas
 import pandas as pd
+import chardet
+
+# Read first few bytes to detect encoding
+with open('dataset.csv', 'rb') as f:
+    rawdata = f.read(10000)  # read first 10 KB
+    result = chardet.detect(rawdata)
+    print(result)
 
 # Step 2: Load the dataset
 # Replace 'your_dataset.csv' with the actual file path
-df = pd.read_csv('dataset.csv')
+df = pd.read_csv('dataset.csv', encoding=result['encoding'])
 
 # Step 3: Print the total number of rows
 print("Total number of rows:", len(df))
