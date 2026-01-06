af38f6bf7ad24539e720bf1aa9b4f4cd5aed7d66
Anusree-Gopinath <53139990+Anusree-Gopinath@users.noreply.github.com>
2026-01-06 13:58:33 +0000

    Evaluation changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Step 1: Import pandas
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # TASK 1
 # Step 2: Load the dataset
@@ -38,3 +40,37 @@ print("y_train shape:", y_train.shape)
 print("y_test shape:", y_test.shape)
 
 # TASK 3
+# Create Logistic Regression model
+model = LogisticRegression(
+    max_iter=1000,
+    solver="lbfgs"
+)
+
+# Train the model
+model.fit(X_train, y_train)
+
+print("Logistic Regression model trained successfully.")
+
+# TASK 4
+# Predictions
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print(cm)
+
+# Extract values
+TN, FP, FN, TP = cm.ravel()
+
+# Sensitivity (Recall)
+sensitivity = TP / (TP + FN)
+print("Sensitivity (Recall):", sensitivity)
+
+# Specificity
+specificity = TN / (TN + FP)
+print("Specificity:", specificity)
