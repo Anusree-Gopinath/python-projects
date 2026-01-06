406a30fecc73d20b1cb39ffb10d32c422cc61da5
Anusree-Gopinath <53139990+Anusree-Gopinath@users.noreply.github.com>
2026-01-06 09:45:15 +0000

    Identify the features and target columns

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 # Step 1: Import pandas
 import pandas as pd
-import chardet
+from sklearn.model_selection import train_test_split
 
-# Read first few bytes to detect encoding
-with open('dataset.csv', 'rb') as f:
-    rawdata = f.read(10000)  # read first 10 KB
-    result = chardet.detect(rawdata)
-    print(result)
-
+# TASK 1
 # Step 2: Load the dataset
-# Replace 'your_dataset.csv' with the actual file path
-df = pd.read_csv('dataset.csv', encoding=result['encoding'])
+df = pd.read_csv('dataset.csv', encoding='UTF-8-SIG')
 
 # Step 3: Print the total number of rows
 print("Total number of rows:", len(df))
@@ -22,3 +16,25 @@ print(df.head())
 # Step 5 (optional): Display basic info about data types and structure
 print("\nDataset Info:")
 print(df.info())
+
+# TASK 2
+
+# Define features and target
+X = df.drop(columns=["Outcome"])
+y = df["Outcome"]
+
+# Split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Confirm shapes
+print("X_train shape:", X_train.shape)
+print("X_test shape:", X_test.shape)
+print("y_train shape:", y_train.shape)
+print("y_test shape:", y_test.shape)
+
+# TASK 3
